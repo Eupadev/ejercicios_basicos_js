a1109c014e65dfe83ec3fcb972b46e23a17155e5
Eupadev <eu.pelaez@gmail.com>
2026-08-20 20:44:23 +0200

    Ejercicio 8

--- Ejercicio8.js
+++ Ejercicio8.js
@@ -6,4 +6,15 @@ Puedes usar este array para probar tu función:
 const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
 function findLongestWord(stringList) {
   // Completar código
+ let longestWord = stringList[0];
+
+  for (let i = 1; i < stringList.length; i++) {
+    if (stringList[i].length > longestWord.length) {
+      longestWord = stringList[i];
+    }
+  }
+
+  return longestWord;
 }
+
+console.log(findLongestWord(avengers));
